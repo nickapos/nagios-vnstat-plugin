95efeafd1a7ba196073746d177c1a5e451faee06
Nick Apostolakis <nickapos@oncrete.gr>
2013-09-15 07:53:20 +0300

    cleaning shell script, removing usage function, setting up if controlling block

--- nagios-vnstat-plugin.sh
+++ nagios-vnstat-plugin.sh
@@ -19,43 +19,24 @@ dailyTransmittingGrade=`vnstat |grep estimated|awk  '{print $3}'|tail -1`
 echo "daily transmitting averate " $dailyTransmitting $dailyTransmittingGrade
 
 
-usage()
-{ 
-    cat << EOF 
-    usage: $0 options 
-    This script will provide statistical networking info based on vnstat.  
-    OPTIONS: -h Show this message 
-    -MD     Present monthly or daily data 
-    -RT     Present receive or transmit data
-EOF 
-}
 
 MON_DAI=
 REC_TRA=
 
-while getopts “ht:MD:RT” OPTION
+while getopts “MD:RT:” OPTION
 do 
     case $OPTION in 
-        h) 
-            usage 
-            exit 1 
-            ;; 
-        t) 
-            TEST=$OPTARG
-            ;; 
         MD) 
             MON_DAI=$OPTARG 
             ;; 
         RD) 
             REC_TRA=$OPTARG 
             ;; 
-        ?) usage 
-            exit 
-            ;; 
     esac 
 done 
 
 if [[ -z $MON_DAI ]] || [[ -z $REC_TRA ]]
-then usage 
+then 
+    echo "Please provide arguments for both -MD and -RD options"
     exit 1 
 fi
